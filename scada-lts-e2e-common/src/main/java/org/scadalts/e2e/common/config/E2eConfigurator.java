d8f0ba300d8682d485d5d652fab5bfd50213917a
Limraj <35842300+Limraj@users.noreply.github.com>
2020-02-03 17:44:15 +0100

    #21 Bug with load configuration

--- scada-lts-e2e-common/src/main/java/org/scadalts/e2e/common/config/E2eConfigurator.java
+++ scada-lts-e2e-common/src/main/java/org/scadalts/e2e/common/config/E2eConfigurator.java
@@ -3,6 +3,9 @@ package org.scadalts.e2e.common.config;
 public class E2eConfigurator {
 
     public static void init(E2eConfig config) {
-
+        E2eConfiguration.authType = config.getAuthType();
+        E2eConfiguration.baseUrl = config.getBaseUrl();
+        E2eConfiguration.password = config.getPassword();
+        E2eConfiguration.userName = config.getUserName();
     }
 }
