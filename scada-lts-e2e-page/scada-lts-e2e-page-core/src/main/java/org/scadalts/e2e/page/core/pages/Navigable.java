6f68d0b5e36c573cb6d7b8dd3bdaff2f6dcd6146
Kamil Jarmusik <hsfgdag8z>
2020-03-31 23:00:22 +0200

    #87 Adding current url to logs - add logging current url

--- scada-lts-e2e-page/scada-lts-e2e-page-core/src/main/java/org/scadalts/e2e/page/core/pages/Navigable.java
+++ scada-lts-e2e-page/scada-lts-e2e-page-core/src/main/java/org/scadalts/e2e/page/core/pages/Navigable.java
@@ -1,17 +1,21 @@
 package org.scadalts.e2e.page.core.pages;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.scadalts.e2e.page.core.components.E2eWebElement;
 
 import static com.codeborne.selenide.Selenide.page;
 
 interface Navigable<T extends MainPageObject<T>> extends GetPage<T> {
 
+    Logger LOGGER = LogManager.getLogger(Navigable.class);
     E2eWebElement getSource();
 
     default T openInNewTab() {
         getSource().openInNewTab();
         T page = getPage();
+        LOGGER.info("current url: {}", page.getCurrentUrl());
         page.waitForCompleteLoad();
         return page;
     }
@@ -19,12 +23,15 @@ interface Navigable<T extends MainPageObject<T>> extends GetPage<T> {
     default T reopen() {
         getSource().click();
         T page = getPage();
+        LOGGER.info("current url: {}", page.getCurrentUrl());
         page.waitForCompleteLoad();
         return page;
     }
 
     default T openPage() {
         getSource().click();
-        return page(getPage());
+        T page = getPage();
+        LOGGER.info("current url: {}", page.getCurrentUrl());
+        return page(page);
     }
 }
