1b820aff295f211725a2b5dffcc0a0d684834405
Limraj <35842300+Limraj@users.noreply.github.com>
2020-06-09 15:44:39 +0200

    #114 Storungs and alarms test API [service] - added: StorungsAndAlarmsServiceObject, AlarmParams, AlarmResponse, PaginationParams

---
+++ scada-lts-e2e-service/scada-lts-e2e-service-impl/src/main/java/org/scadalts/e2e/service/impl/services/alarms/AlarmResponse.java
@@ -0,0 +1,18 @@
+package org.scadalts.e2e.service.impl.services.alarms;
+
+import lombok.*;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Getter
+@Builder
+@ToString
+@XmlRootElement
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlarmResponse {
+
+    private String id;
+
+}
